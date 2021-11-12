182876507e2b766d74921d93c45887b63c8ab6d7
Lucas <lucas_hfonseca@yahoo.com.br>
2021-11-12 11:01:13 -0300

    fix: Correção import

--- src/screens/feedScreen/index.tsx
+++ src/screens/feedScreen/index.tsx
@@ -1,24 +1,24 @@
 import {useNavigation} from '@react-navigation/core';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React, {memo, useCallback, useState} from 'react';
 import {View} from 'react-native';
 import TouchableCard from '../../components/touchableCard';
 import {RootNavigatorParamsList} from '../../navigation/interfaces';
 import {styles} from './styles';
 
-type FeedScreenProps = NativeStackScreenProps<
+type FeedScreenProps = NativeStackNavigationProp<
   RootNavigatorParamsList,
   'FeedScreen'
 >;
 
 const FeedScreen = () => {
   const [value, setValue] = useState<boolean>();
-  const navigation = useNavigation<FeedScreenProps>();
+  const {navigate} = useNavigation<FeedScreenProps>();
 
   const submit = useCallback(() => {
     setValue(!value);
-    navigation.navigate('DetailsScreen');
-  }, [value, navigation]);
+    navigate('DetailsScreen');
+  }, [value, navigate]);
 
   return (
     <View style={styles.scaffold}>
